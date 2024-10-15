d93d8858cd8c7106f1413d2630b5b62d08ccde3e
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-10-15 18:13:01 +0530

    added matchTrade function

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ const ORDERBOOK = {
         orders: {
           user1: 3,
           user2: 3,
-          user3: 6,
         },
       },
     },
@@ -41,7 +40,11 @@ const STOCK_BALANCES = {
   user1: {
     BTC_USDT_10_Oct_2024_9_30: {
       yes: {
-        quantity: 2,
+        quantity: 5,
+        locked: 5,
+      },
+      no: {
+        quantity: 0,
         locked: 0,
       },
     },
@@ -49,8 +52,12 @@ const STOCK_BALANCES = {
   user2: {
     BTC_USDT_10_Oct_2024_9_30: {
       no: {
-        quantity: 3,
-        locked: 4,
+        quantity: 0,
+        locked: 0,
+      },
+      yes: {
+        quantity: 2,
+        locked: 1,
       },
     },
   },
@@ -254,7 +261,132 @@ app.post("/order/sell", (req, res) => {
   res.status(200).json({ message: "Sell order placed and pending" });
 });
 
-app.post("/order/buy", (req, res) => {});
+app.post("/order/buy", (req, res) => {
+  let { userId, stockSymbol, quantity, price, stockType } = req.body;
+
+  let balance = INR_BALANCES[userId].balance;
+  const totalBuyPrice = quantity * price;
+
+  if (balance < totalBuyPrice) {
+    res.status(404).json({ message: "Not enough balance" });
+    return;
+  }
+  if (!(stockSymbol in ORDERBOOK)) {
+    res.status(404).json({ message: "No such stock exists" });
+  }
+
+  const priceToBuy = parseFloat(price) / 100;
+
+  const pricesAvailable = Object.keys(ORDERBOOK[stockSymbol][stockType]).map(
+    (price) => parseFloat(price),
+  );
+  const sortedPrices = pricesAvailable.sort((a, b) => b - a);
+
+  function createReverseSellOrder(priceToMatch, remainingQuantity) {
+    let quantity = remainingQuantity;
+
+    const priceToSell = 10 - priceToMatch;
+
+    const userQuantity =
+      STOCK_BALANCES[userId][stockSymbol]?.[stockType]?.quantity;
+
+    if (!userQuantity) {
+      res.status(404).json({ message: "No stocks to sell" });
+      return;
+    }
+
+    if (userQuantity < quantity) {
+      res.status(404).json({ message: "Not enough stocks" });
+      return;
+    }
+
+    STOCK_BALANCES[userId][stockSymbol][stockType].quantity -= quantity;
+    STOCK_BALANCES[userId][stockSymbol][stockType].locked += quantity;
+
+    const priceString = priceToSell.toString();
+
+    if (!(stockSymbol in ORDERBOOK)) ORDERBOOK[stockSymbol] = {};
+    if (!(stockType in ORDERBOOK[stockSymbol]))
+      ORDERBOOK[stockSymbol][stockType] = {};
+
+    if (priceString in ORDERBOOK[stockSymbol][stockType]) {
+      ORDERBOOK[stockSymbol][stockType][priceString].total += quantity;
+      if (userId in ORDERBOOK[stockSymbol][stockType][priceString].orders) {
+        ORDERBOOK[stockSymbol][stockType][priceString].orders[userId] +=
+          quantity;
+      } else
+        ORDERBOOK[stockSymbol][stockType][priceString].orders[userId] =
+          quantity;
+    } else {
+      ORDERBOOK[stockSymbol][stockType][priceString] = {
+        total: quantity,
+        orders: {
+          [userId]: quantity,
+        },
+      };
+    }
+  }
+
+  function matchTrade(priceToMatch) {
+    const orders = Object.keys(
+      ORDERBOOK[stockSymbol][stockType][priceToMatch].orders,
+    );
+    let remainingQuantity = parseFloat(quantity);
+    let priceToMatchString = priceToMatch.toString();
+    for (let order of orders) {
+      if (
+        ORDERBOOK[stockSymbol][stockType][priceToMatchString].orders[order] <=
+        remainingQuantity
+      ) {
+        let balanceTraded =
+          ORDERBOOK[stockSymbol][stockType][priceToMatchString].orders[order] *
+          priceToMatch *
+          100;
+        let stocksTraded =
+          ORDERBOOK[stockSymbol][stockType][priceToMatchString].orders[order];
+
+        INR_BALANCES[order].balance += balanceTraded;
+        INR_BALANCES[userId].balance -= balanceTraded;
+        STOCK_BALANCES[order][stockSymbol][stockType].locked -= stocksTraded;
+        STOCK_BALANCES[userId][stockSymbol][stockType].quantity += stocksTraded;
+
+        remainingQuantity -= stocksTraded;
+        if (remainingQuantity === 0) break;
+      } else {
+        let balanceTraded = remainingQuantity * priceToMatch * 100;
+        let stocksTraded = remainingQuantity;
+
+        INR_BALANCES[order].balance += balanceTraded;
+        INR_BALANCES[userId].balance -= balanceTraded;
+        STOCK_BALANCES[order][stockSymbol][stockType].locked -= stocksTraded;
+        STOCK_BALANCES[userId][stockSymbol][stockType].quantity += stocksTraded;
+
+        // TODO: its obvios that it will be 0 here
+        remainingQuantity -= stocksTraded;
+        if (remainingQuantity === 0) break;
+      }
+    }
+
+    if (remainingQuantity === 0) return;
+    else {
+      createReverseSellOrder(priceToMatch, remainingQuantity);
+      return;
+    }
+  }
+
+  //lowest match
+  if (priceToBuy > sortedPrices[0]) {
+    try {
+      matchTrade(sortedPrices[0]);
+      // TODO: Add message as per cases
+      res.status(200).json({ message: "Trade executed" });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: "Transaction Failed" });
+      return;
+    }
+  }
+});
 
 app.listen(3000, () => {
   console.log("listening on 3000");
